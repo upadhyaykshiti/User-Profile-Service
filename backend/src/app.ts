482fb4c4e269007882f7d6a46173dad66e92fee7
Kshiti Upadhyay <kshiti.u@kiotel.co.in>
2025-11-10 13:20:43 +0530

    Updating app.ts by adding profile routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth";
+import profileRoutes from "./routes/profile";
+
 
 const app = express();
 
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
+
 
 app.get("/health", (req, res) => res.json({ status: "ok" }));
 
