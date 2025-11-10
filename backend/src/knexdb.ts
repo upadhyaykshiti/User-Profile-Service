8bf311c2e033a66d11eea79206b79ba8bf8098d2
Kshiti Upadhyay <kshiti.u@kiotel.co.in>
2025-11-10 14:37:35 +0530

    Updating knexdb.ts

--- backend/src/knexdb.ts
+++ backend/src/knexdb.ts
@@ -1,5 +1,8 @@
 import knex from 'knex';
-import knexConfig from '../knexfile';
+// import knexConfig from '../knexfile';
+const knexConfig = require("../knexfile.cjs");
+
 
 export const db = knex(knexConfig.development);
 export default db;
+
